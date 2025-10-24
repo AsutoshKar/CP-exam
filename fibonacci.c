fb7a2da921abab38d54662e2dc465ae4d7b2862e
Asutosh Kar <asutoshkar126@gmail.com>
2025-10-24 18:12:18 +0530

    edited

--- fibonacci.c
+++ fibonacci.c
@@ -1,22 +1,34 @@
 // Let's find a Fibonacci series in C
 #include <stdio.h>
 
-int main() {
-    int n, t1 = 0, t2 = 1, nextTerm;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: %d, %d", t1, t2);
-    nextTerm = t1 + t2;
-    for (int i = 3; i <= n; i++) {
-        printf(", %d", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
-        if (nextTerm>=n)
-        {
-            break;
-        }
+int main(void) {
+    int n;
+    int t1 = 0, t2 = 1, nextTerm;
+
+    printf("Enter the number: ");
+    if (scanf("%d", &n) != 1) return 1;
+
+    if (n <= 0) {
+        printf("Enter a positive integer.\n");
+        return 0;
     }
+
+    else if (n == 1) {
+        printf("Fibonacci Series: %d\n", t1);
+        return 0;
+    }
+    else{
+        printf("Fibonacci Series: %d, %d", t1, t2);
+        do {
+            nextTerm = t1 + t2;
+            printf(", %d", nextTerm);
+            t1 = t2;
+            t2 = nextTerm;
+            if (nextTerm > n) 
+                break;
+        } while (1);
+    }
+
     printf("\n");
     return 0;
 }
