364066a49e991bc6b239faacfe6732467ccfccbc
RKd2007-rishu <depressediitian2025@gmail.com>
2025-12-27 17:32:55 +0530

    Update insertion_array.c

---
+++ insertion_array.c
@@ -0,0 +1,25 @@
+//
+include <stdio.h>
+int main() {
+int a[100], n, i, pos, num;
+printf("Enter the size of the array: ");
+scanf("%d", &n);
+printf("Enter %d elements:\n", n);
+for(i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+    printf("enter the element to be added:");
+    scanf("%d", &num);
+    printf("enter the position:");
+    scanf("%d", &pos);
+    for(i=n;i>=pos;i--)
+    {
+        a[i]=a[i-1];
+    }
+    a[i=1]=num;
+}
+for(i=0;i<=n;i++)
+{
+printf("Entered array =%d",a[i]);
+}
+return 0;
+}
